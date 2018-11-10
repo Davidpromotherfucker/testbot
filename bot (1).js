5d85b40db698e00779c511c92b546cb0297010a0
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-10 03:20:30 +0200

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "1";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "509342401085571079"; // ايدي السررفر
+var channel = "509342401085571081";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split("بسم الله الرحمن الرحيم احتزم حبيبي ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login('NTA4NTgwNjk5OTM5NDA1ODI2.DsZF8Q.K3EWKUtVA_-Ub0AXRaaLD_liJJE');
